7430023310c2864443db188903c70c174c9fc526
Ken Flerlage <flerlagekr@gmail.com>
2021-04-28 21:59:49 -0400

    20210428 Lots of mods, including capturing character info.

--- scrape.py
+++ scrape.py
@@ -1,13 +1,32 @@
 #  Written by Ken Flerlage, April, 2021
 #
+#  Note: 
+#  I'm not a web scraping expert, so I realize I am probably doing a lot of manual stuff in this code
+#  that could be improved upon. Please don't be too harsh!!
+#
 #  This code is in the public domain
 
 import codecs
 import requests
 import html
+import time
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from bs4 import BeautifulSoup
+
+#---------------------------------------------------------------------------------------
+# Find the first of one string or another.
+#---------------------------------------------------------------------------------------
+def findOr(string, substring1, substring2, start):
+    pos  = string.find(substring1, start)
+    pos2 = string.find(substring2, start)
+
+    if pos == -1:
+        pos = pos2
+    elif pos2 < pos and pos2 > 0:
+        pos = pos2      
+
+    return pos
 
 #---------------------------------------------------------------------------------------
 # Basic Stuff
@@ -26,6 +45,8 @@ gc = gspread.authorize(credentials)
 #---------------------------------------------------------------------------------------
 # Process Episodes for Each Show
 #---------------------------------------------------------------------------------------
+
+print('Processing episode records.')
 
 # Read the list of shows from the Google sheet
 sheet = gc.open_by_url('https://docs.google.com/spreadsheets/d/1O7EteEDpOc37dQWAS9LD3bNxAOCEbL4MOuVLP5yreyY')
@@ -47,7 +68,7 @@ for i in range(1, len(showList)):
     page = requests.get(pageURL)
 
     if page.status_code==200:
-        print("Downloaded from " + pageURL)
+        #print("Downloaded from " + pageURL)
 
         episodeNum = 0
         episodeString =""
@@ -78,8 +99,11 @@ for i in range(1, len(showList)):
                 if episodeNum == maxEpisode:
                     # We don't want to load any future episodes.
                     found=False
-
+                   
                 text = text[strPos-6:len(text)]
+                text = text.replace('<a class="mw-redirect" href="', '<a href="')
+                text = text.replace('<a class="mw-disambig" href="', '<a href="')
+
                 strPos = text.find(" ")
                 episodeString = text[0:strPos]  
                 episodeString = episodeString.replace(">", "")
@@ -96,6 +120,20 @@ for i in range(1, len(showList)):
                 strPos = text.find("<a href=")
                 strPos2 = text.find('" title="')
                 episodeURL = urlBase + text[strPos+9:strPos2]
+
+                # Correct incorrect URLs on the website
+                if showName=="Law & Order" and episodeNum == 99:
+                    episodeURL = "https://lawandorder.fandom.com/wiki/Guardian_(L%26O)"
+
+                if showName=="Law & Order" and episodeNum == 185:
+                    episodeURL = "https://lawandorder.fandom.com/wiki/Flight_(L%26O)"
+
+                if showName=="Law & Order" and episodeNum == 196:
+                    episodeURL = "https://lawandorder.fandom.com/wiki/Disciple_(L%26O)"
+
+
+                if episodeURL == urlBase:
+                    print("Potential data problem (no URL found) with episode # " + str(episodeCount+1))
 
                 # Parse out the title.
                 strPos = strPos2+9
@@ -116,14 +154,15 @@ for i in range(1, len(showList)):
                 episodeYear = tempText[strPos+9:strPos2]
 
                 # Store all values in an array.
-                episodeMatrix[episodeCount, 0] = showName
-                episodeMatrix[episodeCount, 1] = seasonNum
-                episodeMatrix[episodeCount, 2] = seasonEpisode
-                episodeMatrix[episodeCount, 3] = episodeNum
-                episodeMatrix[episodeCount, 4] = episodeTitle
-                episodeMatrix[episodeCount, 5] = episodeDayMonth
-                episodeMatrix[episodeCount, 6] = episodeYear
-                episodeMatrix[episodeCount, 7] = episodeURL
+                episodeMatrix[episodeCount, 0] = episodeCount+1
+                episodeMatrix[episodeCount, 1] = showName
+                episodeMatrix[episodeCount, 2] = seasonNum
+                episodeMatrix[episodeCount, 3] = seasonEpisode
+                episodeMatrix[episodeCount, 4] = episodeNum
+                episodeMatrix[episodeCount, 5] = episodeTitle
+                episodeMatrix[episodeCount, 6] = episodeDayMonth
+                episodeMatrix[episodeCount, 7] = episodeYear
+                episodeMatrix[episodeCount, 8] = episodeURL
                 episodeCount += 1
 
     else:
@@ -131,7 +170,7 @@ for i in range(1, len(showList)):
         print("Failed to download from " + pageURL)
 
 # Loop through the matrix and write values for a batch update to Google Sheets.
-rangeString = "A2:H" + str(episodeCount+1)
+rangeString = "A2:I" + str(episodeCount+1)
 cell_list = worksheet.range(rangeString)
 
 row = 0
@@ -140,7 +179,7 @@ column = 0
 for cell in cell_list: 
     cell.value = episodeMatrix[row,column]
     column += 1
-    if (column > 7):
+    if (column > 8):
         column=0
         row += 1
 
@@ -149,16 +188,215 @@ worksheet.update_cells(cell_list)
 
 print ("Wrote " + str(episodeCount) + " episode records.")
 
-print("Finished processing all episodes.")
-
 #---------------------------------------------------------------------------------------
 # Process Characters for Each Episode.
 #---------------------------------------------------------------------------------------
 
-# Loop through each episode on the episode sheet.showName
-# Collect main cast members, Recurring cast members, and Guest cast members.
-# Capture the character and actor, as well as both the related URLs
-# URLs will help us to uniquely identify the person.
-# Ignore titles.
-# Do we need to collect biographical details about the characters?
-# Write separate record for each character/episode combination?
+print('Processing character records.')
+
+# Read the list of shows from the Google sheet
+worksheet = sheet.worksheet("Episodes")
+
+urlList = worksheet.col_values(9)
+episodeIDList = worksheet.col_values(1)
+episodeList = worksheet.col_values(6)
+
+characterNum = 0
+characterCount = 0
+characterMatrix = {}
+
+# Loop through each episode and collect information.
+for i in range(1, len(urlList)):
+    pageURL = urlList[i]
+
+    page = requests.get(pageURL)
+
+    if page.status_code==200:
+        soup = BeautifulSoup(page.content, 'html.parser')
+        text = str(soup)
+
+        found = True
+
+        # Loo through the 3 sections -- main, recurring, guest
+        for j in range(1,4):
+            if j==1:
+                characterType = "Main"
+                strPos  = text.lower().find('<span class="mw-headline" id="Main_cast">Main cast</span>'.lower())
+                strPos2 = text.lower().find('<span class="mw-headline" id="Recurring_cast">Recurring cast</span>'.lower())
+
+                if strPos2 < 0:
+                    # Sometimes the recurring section is missing. Use the guest section.
+                    strPos2 = text.lower().find('<span class="mw-headline" id="Guest_cast">Guest cast</span>'.lower())
+
+            elif j==2:
+                characterType = "Recurring"
+                strPos  = text.lower().find('<span class="mw-headline" id="Recurring_cast">Recurring cast</span>'.lower())
+                strPos2 = text.lower().find('<span class="mw-headline" id="Guest_cast">Guest cast</span>'.lower())
+
+                if strPos < 0:
+                    # Sometimes the recurring section is missing.
+                    found = False
+                else:
+                    found = True
+
+            else:
+                characterType = "Guest"
+                strPos  = text.lower().find('<span class="mw-headline" id="Guest_cast">Guest cast</span>'.lower())
+                strPos2 = text.lower().find('<span class="mw-headline" id="References">References</span>'.lower())
+
+                if strPos < 0:
+                    # Sometimes the guest section is missing.
+                    found = False
+                else:
+                    found = True
+
+            characterText = text[strPos:strPos2]
+            startPos = 0
+
+            # Will need to loop through recurring and guest cast as well.
+
+            # Get all the different characters in this section
+            while (found==True):
+                characterNum += 1
+                
+                # Find next character.
+                strPos = findOr(characterText, '<li><a href="', '<li><a class="new"', startPos)
+
+                if strPos == -1:
+                    # Could not find another episode
+                    found=False
+                else:         
+                    found=True
+
+                if found==True:
+                    strPos2 = findOr(characterText, '<li><a href="', '<li><a class="new"', strPos+1)
+
+                    # Set next loop's start string position.
+                    startPos = strPos2
+
+                    tempText = characterText[strPos:strPos2]
+                    tempText = tempText.replace('&#160;as&#160;', ' as ')
+
+                    # Get the Actor URL
+                    strPos = tempText.find('" title=')
+                    actorURL = tempText[13:strPos]
+                    if actorURL[0:22]=='"new" data-uncrawlable':
+                        # No URL
+                        actorURL = ""
+                    else:
+                        actorURL = urlBase + actorURL
+
+                    # Get the Actor Name
+                    strPos = tempText.find('">')
+                    strPos2 = tempText.find("</a>")
+                    actorName = tempText[strPos+2:strPos2]
+
+                    # Get the Character URL
+                    characterURL = ""
+                    tempText = tempText[strPos2:startPos]
+                    tempText = tempText.replace('<a class="mw-redirect" href="', '<a href="')
+                    tempText = tempText.replace('<a class="mw-disambig" href="', '<a href="')
+
+                    linkCount = tempText.count('<a href="') + tempText.count('<a class="new"')
+                    
+                    if linkCount>1:
+                        # Skip first link (title)
+                        strPos = findOr(tempText, '<a href="', '<a class="new"', 0)
+                        strPos = findOr(tempText, '<a href="', '<a class="new"', strPos+1)
+                    else:
+                        strPos = findOr(tempText, '<a href="', '<a class="new"', 0)
+            
+                    if linkCount>0:
+                        strPos2 = tempText.find('" title=', strPos+1)
+                        characterURL = tempText[strPos+9:strPos2]
+                        if characterURL[0:22]=='"new" data-uncrawlable':
+                            # No URL
+                            characterURL = ""
+                        else:
+                            characterURL = urlBase + characterURL
+
+                    # Get the Character Name
+                    tempText2 = tempText
+                    tempText = tempText[strPos2:startPos]
+
+                    strPos = tempText.find('">')
+                    strPos2 = tempText.find("</a>")
+                    characterName = tempText[strPos+2:strPos2]
+
+                    if characterName[0:34] == '<svg class="wds-icon wds-icon-tiny':
+                        # This is the last character and has no URL so it's jumped ahead. Clear the URL and fix the name.
+                        characterURL = ""
+
+                        strPos = tempText2.find('</a> as ')
+                        strPos2 = tempText2.find("</li>")
+                        characterName = tempText2[strPos+8:strPos2]
+
+                    # Clean up problematic text.
+                    if characterName.strip() == "":
+                        characterName = tempText2.replace("</a> ", "")
+                        characterName = characterName.replace("</li>", "")
+                        characterName = characterName.replace("as ", "")
+                        characterName = characterName.replace(" <i>(uncredited)</i>", "")
+                        characterName = characterName.replace("\n", "")
+                        
+                    characterName = characterName.replace("</ul><h", "")      
+                    characterName = characterName.replace("</ul>", "")      
+                    characterName = characterName.replace("<ul>", "")      
+                    characterName = characterName.replace(" (uncredited)", "")      
+                    characterName = characterName.replace("<i>", "")      
+                    characterName = characterName.replace("</i>", "")
+                    characterName = characterName.replace("</li>", "")
+                    characterName = characterName.replace("</a>", "")
+
+                    if characterName == 'Detective Elliot Stabler':
+                        characterName = 'Elliot Stabler'
+                 
+                    if characterName == 'Detective Olivia Benson':
+                        characterName = 'Olivia Benson'
+                 
+                    if len(characterName) == 0:
+                        print("Potential data problem (no character name) with character # " + str(characterCount+1))
+
+                    if len(actorName) == 0:
+                        print("Potential data problem (no actor name) with character # " + str(characterCount+1))
+
+                    if len(characterName) > 50 or len(actorName) > 50:
+                        print("Potential data problem (name too long) with character # " + str(characterCount+1))
+                        characterName = ""
+
+                    # Store all values in an array.
+                    characterMatrix[characterCount, 0] = characterCount+1
+                    characterMatrix[characterCount, 1] = episodeIDList[i]
+                    characterMatrix[characterCount, 2] = characterType
+                    characterMatrix[characterCount, 3] = actorName
+                    characterMatrix[characterCount, 4] = characterName
+                    characterMatrix[characterCount, 5] = actorURL
+                    characterMatrix[characterCount, 6] = characterURL
+
+                    characterCount += 1
+
+    else:
+        # Failed to download the content.
+        print("Failed to download from " + pageURL)        
+
+
+worksheet = sheet.worksheet("Characters")
+
+# Loop through the matrix and write values for a batch update to Google Sheets.
+rangeString = "A2:G" + str(characterCount+1)
+cell_list = worksheet.range(rangeString)
+
+row = 0
+column = 0
+
+for cell in cell_list: 
+    cell.value = characterMatrix[row,column]
+    column += 1
+    if (column > 6):
+        column=0
+        row += 1
+
+# Update in batch 
+worksheet.update_cells(cell_list)
+
+print ("Wrote " + str(characterCount) + " character records.")
